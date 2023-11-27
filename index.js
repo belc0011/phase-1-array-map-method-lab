d1aa6d8b495fc377675934e8f40f5e5262e04569
Monica Garcia <monicairenegarcia@gmail.com>
2023-11-26 17:53:23 -0700

    completed array map method lab

--- index.js
+++ index.js
@@ -12,5 +12,27 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  for (let i = 0; i<tutorials.length; i++) {
+    tutorials[i] = tutorials[i].charAt(0).toUpperCase() + tutorials[i].slice(1);
+  }
+  const newArray = tutorials.map(title => {
+   const arrayOfWords = []
+    for (let j = 0; j < title.length; j++) {
+      debugger
+      if (title.charAt(j) === ' ') {
+        debugger
+        let captureFirstPart = title.slice(0,j);
+        let titleFragment = title.slice(j+1)
+        let capitalizeFirstLetter = titleFragment.charAt(0).toUpperCase();
+        let newTitleFragment = capitalizeFirstLetter + titleFragment.slice(1);
+        title = captureFirstPart + " " + newTitleFragment;
+        debugger
+      }
+    }
+    return title;
+    debugger
+  })
+  return newArray;
+  debugger
 }
+console.log(titleCased());
